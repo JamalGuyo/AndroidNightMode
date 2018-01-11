10d9aa9ea5e55df79472ad3171edd4e3edb7027a
Jamal Guyo <jamalguyo08@gmail.com>
2018-01-11 11:34:46 +0300

    original night mode app

---
+++ app/src/main/java/com/example/root/a201nightmode/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.example.root.a201nightmode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class SettingsActivity extends AppCompatActivity {
+    Switch myswitch;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darktheme);
+        }else{
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        myswitch = findViewById(R.id.myswitch);
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp(){
+        Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
